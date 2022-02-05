76933b9cd0bb84316b5dcb685e65acc83ec681f3
Brad Hamm <bradjh83@gmail.com>
2022-02-05 13:56:47 -0800

    Init practice blockchain

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = '';
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, Date.now(), 'Genesis block', '0');
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let coin = new Blockchain();
+coin.addBlock(new Block(1, Date.now(), {amount: 4}));
+coin.addBlock(new Block(2, Date.now(), {amount: 10}));
+
+console.log(JSON.stringify(coin, null, 4));
